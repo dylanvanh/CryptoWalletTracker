653201ccfbe572ebf227953e38d3b2de9d2d021d
Sparkling Water <dylanvh45@gmail.com>
2021-12-28 15:45:29 +0200

    Removed useless header bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Fragment } from "react/cjs/react.production.min";
 import classes from "./App.module.css";
 import Main from "./components/BodyDisplay/Main";
-import Header from "./components/HeaderBar/Header";
+import Navbar from "./components/HeaderBar/Navbar/Navbar";
 import Footer from "./components/FooterBar/Footer";
 
 
 const App = () => {
   return (
     <div className={classes.container}>
-      <Header className={classes.header} />
+      <Navbar/>
       <Main className={classes.main} />
       <Footer className={classes.footer} />
     </div>
