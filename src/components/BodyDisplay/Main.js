787f10cf6c4c5f46dd8dd765920c8d2a342af63a
Sparkling Water <dylanvh45@gmail.com>
2022-01-16 23:44:31 +0200

    cleanup

--- src/components/BodyDisplay/Main.js
+++ src/components/BodyDisplay/Main.js
@@ -7,7 +7,7 @@ import GasStats from "./GasStats/GasDisplay";
 //handles all the body components (BodyDisplay)
 const MainDisplay = (props) => {
 
-  console.log('main = ',props.tokenData);
+  console.log('main = ', props.tokenData);
 
   return (
     <main className={classes["main-container"]}>
@@ -15,7 +15,7 @@ const MainDisplay = (props) => {
         <TransactionsDisplay />
       </div>
       <div className={classes["portfolio-display-container"]}>
-        <PortfolioDisplay tokenData={props.tokenData}/>
+        <PortfolioDisplay tokenData={props.tokenData} />
       </div>
       <div className={classes["gas-stats-container"]}>
         <GasStats />
