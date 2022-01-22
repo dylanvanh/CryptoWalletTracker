e93df5cea8ec79a201061028223b7bda41760d94
Sparkling Water <dylanvh45@gmail.com>
2022-01-22 23:31:24 +0200

    pc change

--- src/AppBackup.js
+++ src/AppBackup.js
@@ -49,19 +49,85 @@ const App = () => {
       'accept': 'application/json',
       'X-API-Key': `${process.env.REACT_APP_X_API_KEY}`,
     }
+    
+    const geckoApiHeader = {
+      'accept': 'application/json',
+    }
 
     setIsLoading(true);
     setError(null);
     console.log('fetchwalletdatahandler!')
 
     try {
-      const responseErc20 = await fetch(moralis_api_call_erc20, {
+      const responseErc20 = await fetch(moralis_api_call_native, {
         headers: moralisApiHeader,
       });
-      //native token data
-      const erc20Data = await responseErc20.json();
 
-      
+      const responseNative = await fetch(moralis_api_call_erc20, {
+        headers: moralisApiHeader,
+      });
+
+      if (!responseErc20.ok) {
+        throw new Error('Error fetching token data')
+      }
+
+      //swap around the native and erc20 -> wrong (not changing now due to a different bug)
+      const nativeData = await responseErc20.json();
+      const erc20Data = await responseNative.json();
+      console.log('ue = ', nativeData)
+      console.log('ue = ', erc20Data);
+
+      //converts fetched tokenData data into improved format
+      const transformedTokenData = erc20Data.map((tokenData) => {
+        return {
+          tokenAddress: tokenData.token_address,
+          name: tokenData.name,
+          balance: tokenData.balance,
+          decimals: tokenData.decimals,
+          symbol: tokenData.symbol,
+          price: null,
+          dayChange: null,
+          totalValue: null,
+        };
+      });
+
+      //handles fetching prices for the different balances fetched
+      //allows filtering out the spam tokens (ones without values)
+      const addresses = transformedTokenData.map(
+        token => token.tokenAddress
+      );
+      const combinedAddresses = addresses.join('%2C')
+      const api_prices = `https://api.coingecko.com/api/v3/simple/token_price/polygon-pos?contract_addresses=${combinedAddresses}&vs_currencies=usd&include_24hr_change=true`
+
+      const responsePrices = await fetch(api_prices, {
+        headers: geckoApiHeader,
+      });
+
+      const priceData = await responsePrices.json();
+
+      //converts fetched price data into improved format
+      const convertedPrices = Object.entries(priceData)
+      const newPrices = convertedPrices.map((data) => {
+        return {
+          tokenAddress: data[0],
+          price: data[1]['usd'],
+          change: data[1]['usd_24h_change'],
+        }
+      })
+
+      //adds the price,24hourchange in price to data
+      transformedTokenData.forEach((tokenData) => {
+        let foundAddress = newPrices.find((priceData) => priceData.tokenAddress === tokenData.tokenAddress)
+        if (foundAddress != undefined) {
+          tokenData.price = foundAddress['price'];
+          tokenData.dayChange = foundAddress['change'];
+        }
+      });
+
+      //stores data into state variable
+      setTokenData(transformedTokenData)
+      console.log('ttd', transformedTokenData)
+
     } catch (error) {
       setError(error.message);
     }
