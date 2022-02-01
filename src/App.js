beae29bb1ff784c034d76a748a566e2ec37e5eda
Sparkling Water <dylanvh45@gmail.com>
2022-02-01 23:04:42 +0200

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -9,54 +9,20 @@ import useFetch from './hooks/useFetch';
 
 const App = () => {
   const userCtx = useContext(UserContext);
-
-  //fetches token balances
-  // const [erc20TokenData, setErc20TokenData] = useState([]);
-  // const [nativeTokenData,setNativeTokenData] = useState([]);
-  const [isFirstTimeLoad, setIsFirstTimeLoad] = useState(true);
-
-  // const selectedWallet = '0xa9ac72E3BbD107eC40546Fc1C68c5e40fc7A9DD9';
-
-  //displays the first wallet added as the default wallet on first load
-  // var selectedWallet = null;
-
-  // if (userCtx.wallets != null) {
-  //   if (isFirstTimeLoad) {
-  //     selectedWallet = userCtx.wallets[0];
-  //     userCtx.selectWallet(selectedWallet);
-  //     setIsFirstTimeLoad(false);
-  //   } else {
-  //     selectedWallet = userCtx.selectedWallet;
-  //   }
-  // }
-
+  
   //SELECTED WALLET IS GIVING NULL!
   const { erc20TokenData, nativeTokenData, isLoading, error } = useFetch();
   console.log(erc20TokenData)
-
-  // if (erc20TokenData.length > 0) {
-  //   content = <Main erc20TokenData={erc20TokenData} nativeTokenData={nativeTokenData} />
-  // }
-
-  // if (error) {
-  //   content = <h1>AN ERROR HAS OCCURED!</h1>
-
-  //   if (userCtx.wallets != null) {
-  //     content = <h1>No added wallets</h1>
-  //   }
-  // }
-
-  // if (isLoading) {
-  //   content = <Card><h1 className={classes.loading}>Loading...</h1></Card>;
-  // }
-
   return (
     <>
       {userCtx.isModalShowing && <AddWalletModal />}
       <Navbar />
-      {isLoading && <Card><h1 className={classes.loading}>Loading...</h1></Card>}
-      {error && <h1>Error encountered</h1>}
-      {!isLoading && <Main erc20TokenData={erc20TokenData} nativeTokenData={nativeTokenData} />}
+      {!userCtx.selectedWallet && <Card><h1>NO WALLET SELECTED</h1></Card>}
+      {userCtx.selectedWallet && <div>
+        {isLoading && <Card><h1 className={classes.loading}>Loading...</h1></Card>}
+        {error && <h1>Error encountered</h1>}
+        {!isLoading && <Main erc20TokenData={erc20TokenData} nativeTokenData={nativeTokenData} />}
+      </div>}
     </>
   );
 
