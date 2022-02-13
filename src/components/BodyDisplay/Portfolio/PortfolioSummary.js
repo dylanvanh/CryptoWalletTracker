fb3d84b98971b198c91170b05a49fcec7eb269db
Sparkling Water <dylanvh45@gmail.com>
2022-02-14 01:08:46 +0200

    Added Chain icon to portfolio summary

--- src/components/BodyDisplay/Portfolio/PortfolioSummary.js
+++ src/components/BodyDisplay/Portfolio/PortfolioSummary.js
@@ -1,8 +1,7 @@
 import classes from './PortfolioSummary.module.css';
 import ArrowUp from '../../../icons/token/arrowupgreen.svg';
 import ArrowDown from '../../../icons/token/arrowdownred.svg';
-import WalletIcon from '../../HeaderBar/WalletIcon';
-import { Wallet } from 'ethers';
+import Ethereum from '../../../icons/chains/ethereum.svg'
 
 
 const PortfolioSummary = (props) => {
@@ -14,25 +13,28 @@ const PortfolioSummary = (props) => {
   return (
     <>
       <div className={classes['portfolio-summary']}>
-        <div className={classes['top-row']}>
+        <div className={classes['top-area']}>
+          <div className={classes["top-row"]}>
           <h2 className={classes['balance-title']}>Your Balance</h2>
-          <div className={classes['row-2-container']}>
-            <h1 className={classes['total-value']}>${totalValue}</h1>
-          </div>
+          <img src={Ethereum} className='chain'></img>
         </div>
-        <div className={classes['bottom-row']}>
-          <div className={classes['daily-profit-container']}>
-            <h4 className={classes['daily-profit-title']}>Todays Profit</h4>
-            <h3 className={classes['daily-profit-amount']}>+$200.04</h3>
-          </div>
-          <div className={classes['daily-percentage-container']}>
-            <div className={classes['daily-percentage']}>
-              <h5>+2.1%</h5>
-              <img src={ArrowUp}></img>
-            </div>
+        <div className={classes['row-2-container']}>
+          <h1 className={classes['total-value']}>${totalValue}</h1>
+        </div>
+      </div>
+      <div className={classes['bottom-row']}>
+        <div className={classes['daily-profit-container']}>
+          <h4 className={classes['daily-profit-title']}>Todays Profit</h4>
+          <h3 className={classes['daily-profit-amount']}>+$200.04</h3>
+        </div>
+        <div className={classes['daily-percentage-container']}>
+          <div className={classes['daily-percentage']}>
+            <h5>+2.1%</h5>
+            <img src={ArrowUp}></img>
           </div>
         </div>
       </div>
+    </div>
     </>
   );
 };
