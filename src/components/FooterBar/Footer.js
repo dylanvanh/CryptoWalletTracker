f57c203f59df2e5581b0ea84174226451d40f760
Sparkling Water <dylanvh45@gmail.com>
2022-02-13 11:46:52 +0200

    Altered Footer

--- src/components/FooterBar/Footer.js
+++ src/components/FooterBar/Footer.js
@@ -1,19 +1,23 @@
 import classes from "./Footer.module.css";
-import { ReactComponent as Twitter } from "../../icons/footer/twitter.svg";
-import { ReactComponent as Github } from "../../icons/footer/github.svg";
-import { ReactComponent as Linkedin } from "../../icons/footer/linkedin.svg";
-import { ReactComponent as Figma } from "../../icons/footer/figma.svg";
+// import { ReactComponent as Twitter } from "../../icons/footer/twitter.svg";
+// import { ReactComponent as Github } from "../../icons/footer/github.svg";
+// import { ReactComponent as Linkedin } from "../../icons/footer/linkedin.svg";
+// import { ReactComponent as Figma } from "../../icons/footer/figma.svg";
 
+import Twitter from "../../icons/footer/twitter.svg";
+import Github from "../../icons/footer/github.svg";
+import Linkedin from "../../icons/footer/linkedin.svg";
+import Figma from "../../icons/footer/figma.svg";
 
 
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <svg viewBox="0 0 100 100"><Twitter /></svg>
-      <svg viewBox="0 0 100 100"><Github /></svg>
-      <svg viewBox="0 0 100 100"><Linkedin /></svg>
-      <svg viewBox="0 0 100 100"> <Figma /></svg>
+      <img src={Twitter} />
+      <img src={Github} />
+      <img src={Linkedin} />
+      <img src={Figma} />
     </footer>
   );
 };
