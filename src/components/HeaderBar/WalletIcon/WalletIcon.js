9b978ce92813e5bd54327abfc669d356c093b465
Sparkling Water <dylanvh45@gmail.com>
2022-02-22 14:35:21 +0200

    Fixed bug with walletIcon when no added wallet

--- src/components/HeaderBar/WalletIcon/WalletIcon.js
+++ src/components/HeaderBar/WalletIcon/WalletIcon.js
@@ -2,16 +2,25 @@ import classes from './WalletIcon.module.css';
 import { useContext } from 'react';
 import BlockieImage from './BlockieImage';
 import UserContext from '../../../context/UserContext';
+import Unknown from '../../../icons/token/questionmark.svg';
 
 const WalletIcon = () => {
 
   const userCtx = useContext(UserContext);
 
-  return (
-    <div className={classes['wallet-icon']}>
-      <BlockieImage address={userCtx.selectedWallet} diameter={50} />
-    </div>
-  )
+  if (userCtx.selectedWallet == null || userCtx.selectedWallet == undefined) {
+    return (
+      <div className={classes['wallet-icon']}>
+        <img src={Unknown} />
+      </div>
+    )
+  } else {
+    return (
+      <div className={classes['wallet-icon']}>
+        <BlockieImage address={userCtx.selectedWallet} diameter={50} />
+      </div>
+    )
+  }
 }
 
 export default WalletIcon;
